f8663284b867526aa0c98b22261ea19568396eff
RENAN CALDEIRA DE ANDRADE <113741243+Renan06081989@users.noreply.github.com>
2023-03-10 10:14:57 -0300

    Add files via upload
    PROJETO PARA DISCIPLINA DE LÓGICA DE PROGRAMAÇÃO (SECRETÁRIA CLÍNICA) USANDO LINGUAGEM PYTHON

---
+++ CLÍNICA 2 RENAN CALDEIRA DE ANDRADE.py
@@ -0,0 +1,384 @@
+oSegManha = ["Mauro", "Ana"]
+oTerManha = ["Lucas", "Ana"]
+oQuaManha = ["Mauro", "Ana"]
+oQuiManha = ["Lucas", "Ana"]
+oSexManha = ["Mauro", "Ana"]
+
+oSegTarde = ["Mauro", "Ana"]
+oTerTarde = ["Lucas", "Ana"]
+oQuaTarde = ["Mauro", "Ana"]
+oQuiTarde = ["Lucas", "Ana"]
+oSexTarde = ["Lucas", "Ana"]
+
+oSegNoite = ["Mauro"]
+oTerNoite = ["SEM MÉDICO"]
+oQuaNoite = ["SEM MÉDICO"]
+oQuiNoite = ["Lucas"]
+oSexNoite = ["Mauro", "Lucas"]
+
+cSegManha = ["Cláudio", "Carla"]
+cTerManha = ["Carla"]
+cQuaManha = ["Cláudio"]
+cQuiManha = ["Rita"]
+cSexManha = ["SEM MÉDICO"]
+
+cSegTarde = ["Carla"]
+cTerTarde = ["Cláudio", "Carla"]
+cQuaTarde = ["Rita", "Cláudio"]
+cQuiTarde = ["Rita"]
+cSexTarde = ["Rita"]
+
+cSegNoite = ["Cláudio", "Carla"]
+cTerNoite = ["Cláudio", "Carla"]
+cQuaNoite = ["Rita"]
+cQuiNoite = ["Rita"]
+cSexNoite = ["Rita"]
+
+pSegManha = ["Antônio", "Elis"]
+pTerManha = ["Ruan", "Antônio"]
+pQuaManha = ["Antônio"]
+pQuiManha = ["Ruan"]
+pSexManha = ["Ruan", "Elis"]
+
+pSegTarde = ["Alice", "Antônio", "Elis"]
+pTerTarde = ["Ruan", "Antônio"]
+pQuaTarde = ["Alice", "Antônio"]
+pQuiTarde = ["Ruan"]
+pSexTarde = ["Alice", "Ruan", "Elis"]
+
+pSegNoite = ["Alice", "Elis"]
+pTerNoite = ["SEM MÉDICO"]
+pQuaNoite = ["Alice"]
+pQuiNoite = ["SEM MÉDICO"]
+pSexNoite = ["Alice", "Elis"]
+
+oSegManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+oTerManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+oQuaManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+oQuiManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+oSexManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+
+oSegTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+oTerTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+oQuaTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+oQuiTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+oSexTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+
+oSegNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+oTerNoiteHorarios = [["SEM HORÁRIO"]]
+oQuaNoiteHorarios = [["SEM HORÁRIO"]]
+oQuiNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+oSexNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"], ["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+
+oManha = [oSegManhaHorarios, oTerManhaHorarios, oQuaManhaHorarios, oQuiManhaHorarios, oSexManhaHorarios]
+oTarde = [oSegTardeHorarios, oTerTardeHorarios, oQuaTardeHorarios, oQuiTardeHorarios, oSexTardeHorarios]
+oNoite = [oSegNoiteHorarios, oTerNoiteHorarios, oQuaNoiteHorarios, oQuiNoiteHorarios, oSexNoiteHorarios]
+
+ofta = [oManha, oTarde, oNoite]
+
+cSegManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+cTerManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+cQuaManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+cQuiManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+cSexManhaHorarios = [["SEM HORÁRIO"]]
+
+cSegTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+cTerTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+cQuaTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+cQuiTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+cSexTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+
+cSegNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"], ["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+cTerNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"], ["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+cQuaNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+cQuiNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+cSexNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+
+cManha = [cSegManhaHorarios, cTerManhaHorarios, cQuaManhaHorarios, cQuiManhaHorarios, cSexManhaHorarios]
+cTarde = [cSegTardeHorarios, cTerTardeHorarios, cQuaTardeHorarios, cQuiTardeHorarios, cSexTardeHorarios]
+cNoite = [cSegNoiteHorarios, cTerNoiteHorarios, cQuaNoiteHorarios, cQuiNoiteHorarios, cSexNoiteHorarios]
+
+cardio = [cManha, cTarde, cNoite]
+
+pSegManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+pTerManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+pQuaManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+pQuiManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+pSexManhaHorarios = [["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"], ["08:00 HORAS", "09:00 HORAS", "10:00 HORAS", "11:00 HORAS"]]
+
+pSegTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+pTerTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+pQuaTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+pQuiTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+pSexTardeHorarios = [["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"], ["14:00 HORAS", "15:00 HORAS", "16:00 HORAS", "17:00 HORAS"]]
+
+pSegNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"], ["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+pTerNoiteHorarios = [["SEM HORÁRIO"]]
+pQuaNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+pQuiNoiteHorarios = [["SEM HORÁRIO"]]
+pSexNoiteHorarios = [["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"], ["18:00 HORAS", "19:00 HORAS", "20:00 HORAS"]]
+
+pManha = [pSegManhaHorarios, pTerManhaHorarios, pQuaManhaHorarios, pQuiManhaHorarios, pSexManhaHorarios]
+pTarde = [pSegTardeHorarios, pTerTardeHorarios, pQuaTardeHorarios, pQuiTardeHorarios, pSexTardeHorarios]
+pNoite = [pSegNoiteHorarios, pTerNoiteHorarios, pQuaNoiteHorarios, pQuiNoiteHorarios, pSexNoiteHorarios]
+
+pedi = [pManha, pTarde, pNoite]
+
+  
+def menu():
+    print('''
+    |---------------------------- MENU INICIAL---------------------------------|   
+    | (A) Escolha médico para ver horários disponíveis                         |
+    | (B) Escolha especialidade para ver horários disponíveis                  |
+    | (C) Escolha dia da semana e especialidade para saber horários disponíveis|
+    | (D) Agendar horário                                                      |
+    | (E) Sair                                                                 |                     
+    |--------------------------------------------------------------------------|
+    ''')
+
+    a = input("Digite uma opção: ").upper()
+
+    while a < "A" or a > "E":
+        a = input("Digite uma opção válida: ").upper()
+
+    return a
+
+def medicos():
+    print('''
+|--------QUADRO DE MÉDICOS--------|   
+| (1) MAURO                       |
+| (2) LUCAS                       |
+| (3) ANA                         |
+| (4) RITA                        |
+| (5) CLÁUDIO                     |
+| (6) CARLA                       |
+| (7) ALICE                       |
+| (8) RUAN                        |
+| (9) ANTÔNIO                     |
+| (10) ELIS                       |
+|---------------------------------|
+    ''')
+
+    x = int(input("Digite o número do médico: "))
+
+    while x < 1 or x > 10:
+        x = int(input("Digite uma opção válida: "))
+
+    if x == 1:
+        print(f"\nMauro\n\nSeg {oSegManhaHorarios[0]}\n    {oSegTardeHorarios[0]}\n    {oSegNoiteHorarios[0]}\nQua {oQuaManhaHorarios[0]}\n    {oQuaTardeHorarios[0]}")
+    elif x == 2:
+        print(f"\nAna\n\n\nSeg {oSegManhaHorarios[1]}\n    {oSegTardeHorarios[1]}\nTer {oTerManhaHorarios[1]}\n    {oTerTardeHorarios[1]}\nQua {oQuaManhaHorarios[1]}\n    {oQuaTardeHorarios[1]}\nQui {oQuiManhaHorarios[1]}\n    {oQuiTardeHorarios[1]}\nSex {oSexManhaHorarios[1]}\n    {oSexTardeHorarios[1]}")
+    elif x == 3:
+        print(f"\nLucas\n\nTer {oTerManhaHorarios[0]}\n    {oTerTardeHorarios[0]}\nQui {oQuiManhaHorarios[0]}\n    {oQuiTardeHorarios[0]}\n    {oQuiNoiteHorarios[0]}\nSex {oSexTardeHorarios[0]}\n    {oSexNoiteHorarios[1]}")
+    elif x == 4:
+        print(f"\nCláudio\n\nSeg {cSegManhaHorarios[0]}\n    {cSegNoiteHorarios[0]}\nTer {cTerTardeHorarios[0]}\n    {cTerNoiteHorarios[0]}\nQua {cQuaManhaHorarios[0]}\n    {cQuaTardeHorarios[1]}")
+    elif x == 5:
+        print(f"\nCarla\n\nSeg {cSegManhaHorarios[1]}\n    {cSegTardeHorarios[0]}\n    {cSegNoiteHorarios[1]}\nTer {cTerManhaHorarios[0]}\n    {cTerTardeHorarios[1]}\n    {cTerNoiteHorarios[1]}")
+    elif x == 6:
+        print(f"\nRita\n\nQua {cQuaTardeHorarios[0]}\n    {cQuaNoiteHorarios[0]}\nQui {cQuiManhaHorarios[0]}\n    {cQuiTardeHorarios[0]}\n    {cQuiNoiteHorarios[0]}\nSex {cSexTardeHorarios[0]}\n    {cSexNoiteHorarios[0]}")
+    elif x == 7:
+        print(f"\nAntônio\n\nSeg {pSegManhaHorarios[0]}\n    {pSegTardeHorarios[1]}\nTer {pTerManhaHorarios[1]}\n    {pTerTardeHorarios[1]}\nQua {pQuaManhaHorarios[0]}\n    {pQuaTardeHorarios[1]}")
+    elif x == 8:
+        print(f"\nRuan\n\nTer {pTerManhaHorarios[0]}\n    {pTerTardeHorarios[0]}\nQui {pQuiManhaHorarios[0]}\n    {pQuiTardeHorarios[0]}\nSex {pSexManhaHorarios[0]}\n    {pSexTardeHorarios[1]}")
+    elif x == 9:
+        print(f"\nElis\n\nSeg {pSegManhaHorarios[1]}\n    {pSegTardeHorarios[2]}\n    {pSegNoiteHorarios[1]}\nSex {pSexManhaHorarios[1]}\n    {pSexTardeHorarios[2]}\n    {pSexNoiteHorarios[1]}")
+    else:
+        print(f"\nAlice\n\nSeg {pSegTardeHorarios[0]}\n    {pSegNoiteHorarios[0]}\nQua {pQuaTardeHorarios[0]}\n    {pQuaNoiteHorarios[0]}\nSex {pSexTardeHorarios[0]}\n    {pSexNoiteHorarios[0]}")
+
+
+
+
+
+def especialidades():
+    print('''
+|----------ESPECIALIDADES---------|   
+| (1) OFTALMOLOGISTA              |
+| (2) CARDIOLOGISTA               |
+| (3) PEDIATRA                    |                    
+|---------------------------------|
+    ''')
+
+    a = int(input("Digite a especialidade: "))
+
+    while a < 1 or a > 3:
+        a = int(input("Digite a especialidade: "))
+
+    if a == 1:
+        print("""
+|------------MÉDICOS--------------|   
+| (1) MAURO                       |
+| (2) ANA                         |
+| (3) LUCAS                       |                    
+|---------------------------------|
+""")
+    elif a == 2:
+        print("""
+|------------MÉDICOS--------------|   
+| (1) RITA                        |
+| (2) CLÁUDIO                     |
+| (3) CARLA                       |                    
+|---------------------------------|
+""")
+    else:
+        print("""
+|------------MÉDICOS--------------|   
+| (1) ALICE                       |
+| (2) RUAN                        |
+| (3) ANTONIO                     |
+| (4) ELIS                        |        
+|---------------------------------|
+""")
+    x = int(input("Digite o médico: "))
+    
+    if a == 3:
+        while x < 1 or x > 4:
+            x = int(input("Digite o médico: "))
+    else:
+        while x < 1 or x > 3:
+            x = int(input("Digite o médico: "))
+
+    if a == 1:
+        if x == 1:
+            print(f"\nSeg {oSegManhaHorarios[0]}\n    {oSegTardeHorarios[0]}\n    {oSegNoiteHorarios[0]}\nQua {oQuaManhaHorarios[0]}\n    {oQuaTardeHorarios[0]}")
+        elif x == 2:
+            print(f"\nSeg {oSegManhaHorarios[1]}\n    {oSegTardeHorarios[1]}\nTer {oTerManhaHorarios[1]}\n    {oTerTardeHorarios[1]}\nQua {oQuaManhaHorarios[1]}\n    {oQuaTardeHorarios[1]}\nQui {oQuiManhaHorarios[1]}\n    {oQuiTardeHorarios[1]}\nSex {oSexManhaHorarios[1]}\n    {oSexTardeHorarios[1]}")
+        else:
+            print(f"\nTer {oTerManhaHorarios[0]}\n    {oTerTardeHorarios[0]}\nQui {oQuiManhaHorarios[0]}\n    {oQuiTardeHorarios[0]}\n    {oQuiNoiteHorarios[0]}\nSex {oSexTardeHorarios[0]}\n    {oSexNoiteHorarios[1]}")
+    elif a == 2:
+        if x == 1:
+            print(f"\nSeg {cSegManhaHorarios[0]}\n    {cSegNoiteHorarios[0]}\nTer {cTerTardeHorarios[0]}\n    {cTerNoiteHorarios[0]}\nQua {cQuaManhaHorarios[0]}\n    {cQuaTardeHorarios[1]}")
+        elif x == 2:
+            print(f"\nSeg {cSegManhaHorarios[1]}\n    {cSegTardeHorarios[0]}\n    {cSegNoiteHorarios[1]}\nTer {cTerManhaHorarios[0]}\n    {cTerTardeHorarios[1]}\n    {cTerNoiteHorarios[1]}")
+        else:
+            print(f"\nQua {cQuaTardeHorarios[0]}\n    {cQuaNoiteHorarios[0]}\nQui {cQuiManhaHorarios[0]}\n    {cQuiTardeHorarios[0]}\n    {cQuiNoiteHorarios[0]}\nSex {cSexTardeHorarios[0]}\n    {cSexNoiteHorarios[0]}")
+    else:
+        if x == 1:
+            print(f"\nSeg {pSegManhaHorarios[0]}\n    {pSegTardeHorarios[1]}\nTer {pTerManhaHorarios[1]}\n    {pTerTardeHorarios[1]}\nQua {pQuaManhaHorarios[0]}\n    {pQuaTardeHorarios[1]}")
+        elif x == 2:
+            print(f"\nTer {pTerManhaHorarios[0]}\n    {pTerTardeHorarios[0]}\nQui {pQuiManhaHorarios[0]}\n    {pQuiTardeHorarios[0]}\nSex {pSexManhaHorarios[0]}\n    {pSexTardeHorarios[1]}")
+        elif x == 3:
+            print(f"\nSeg {pSegManhaHorarios[1]}\n    {pSegTardeHorarios[2]}\n    {pSegNoiteHorarios[1]}\nSex {pSexManhaHorarios[1]}\n    {pSexTardeHorarios[2]}\n    {pSexNoiteHorarios[1]}")
+        else:
+            print(f"\nSeg {pSegTardeHorarios[0]}\n    {pSegNoiteHorarios[0]}\nQua {pQuaTardeHorarios[0]}\n    {pQuaNoiteHorarios[0]}\nSex {pSexTardeHorarios[0]}\n    {pSexNoiteHorarios[0]}")
+
+def esp():
+    print('''
+|----------ESPECIALIDADES---------|   
+| (1) OFTALMOLOGISTA              |
+| (2) CARDIOLOGISTA               |
+| (3) PEDIATRA                    |                    
+|---------------------------------|
+    ''')
+
+    a = int(input("Digite a especialidade: "))
+
+    while a < 1 or a > 3:
+        a = int(input("Digite a especialidade: "))
+
+    return a
+
+def dia():
+    print('''
+|----------DIAS DA SEMANA---------|   
+| (1) SEGUNDA                     |
+| (2) TERÇA                       |
+| (3) QUARTA                      |
+| (4) QUINTA                      |
+| (5) SEXTA                       |                 
+|---------------------------------|
+    ''')
+    dia = int(input("Digite o dia: "))
+
+    while dia < 1 or dia > 5:
+        dia = int(input("Digite o dia válido: "))
+
+    return dia
+
+
+def diaEsp(esp, dia):
+    if esp == 1 and dia == 1:
+        print(f"\nMauro\n{oSegManhaHorarios[0]}\n{oSegTardeHorarios[0]}\noSegNoiteHorarios[0]\nAna \n{oSegManhaHorarios[1]}\n{oSegTardeHorarios[1]}")
+    elif esp == 1 and dia == 2:
+        print(f"\nAna\n{oTerManhaHorarios[1]}\n{oTerTardeHorarios[1]}\nLucas\n{oTerManhaHorarios[0]}\n{oTerTardeHorarios[0]}")
+    elif esp == 1 and dia == 3:
+        print(f"\nMauro\n{oQuaManhaHorarios[0]}\n{oQuaTardeHorarios[0]}\nAna\n{oQuaManhaHorarios[1]}\n{oQuaTardeHorarios[1]}")
+    elif esp == 1 and dia == 4:
+        print(f"\nAna\n{oQuiManhaHorarios[1]}\n{oQuiTardeHorarios[1]}\nLucas\n{oQuiManhaHorarios[0]}\n{oQuiTardeHorarios[0]}\n{oQuiNoiteHorarios[0]}")
+    elif esp == 1 and dia == 5:
+        print(f"\nAna\n{oSexManhaHorarios[1]}\n{oSexTardeHorarios[1]}\nLucas\n{oSexTardeHorarios[0]}\n{oSexNoiteHorarios[1]}")
+    elif esp == 2 and dia == 1:
+        print(f"\nCláudio\n{cSegManhaHorarios[0]}\n{cSegNoiteHorarios[0]}\nCarla\n{cSegManhaHorarios[1]}\n{cSegTardeHorarios[0]}\n{cSegNoiteHorarios[1]}")
+    elif esp == 2 and dia == 2:
+        print(f"\nCláudio\n{cTerTardeHorarios[0]}\n{cTerNoiteHorarios[0]}\nCarla\n{cTerManhaHorarios[0]}\n{cTerTardeHorarios[1]}\n{cTerNoiteHorarios[1]}")
+    elif esp == 2 and dia == 3:
+        print(f"\nCláudio\n{cQuaManhaHorarios[0]}\n{cQuaTardeHorarios[1]}\nRita\n{cQuaTardeHorarios[0]}\n{cQuaNoiteHorarios[0]}")
+    elif esp == 2 and dia == 4:
+        print(f"\nRita\n{cQuiManhaHorarios[0]}\n{cQuiTardeHorarios[0]}\n{cQuiNoiteHorarios[0]}")
+    elif esp == 2 and dia == 5:
+        print(f"\nRita\n{cSexTardeHorarios[0]}\n{cSexNoiteHorarios[0]}")
+    elif esp == 3 and dia == 1:
+        print(f"\nAntônio\n{pSegManhaHorarios[0]}\n{pSegTardeHorarios[1]}\nElis\n{pSegManhaHorarios[1]}\n{pSegTardeHorarios[2]}\nAlice\n{pSegTardeHorarios[0]}\n{pSegNoiteHorarios[0]}")
+    elif esp == 3 and dia == 2:
+        print(f"\nAntônio\n{pTerManhaHorarios[1]}\n{pTerTardeHorarios[1]}\nRuan\n{pTerManhaHorarios[0]}\n{pTerTardeHorarios[0]}")
+    elif esp == 3 and dia == 3:
+        print(f"\nAntônio\n{pQuaManhaHorarios[0]}\n{pQuaTardeHorarios[1]}\nAlice\n{pQuaTardeHorarios[0]}\n{pQuaNoiteHorarios[0]}")
+    elif esp == 3 and dia == 4:
+        print(f"\nRuan\n{pQuiManhaHorarios[0]}\n{pQuiTardeHorarios[0]}")
+    elif esp == 3 and dia == 5:
+        print(f"\nRuan\n{pSexManhaHorarios[0]}\n{pSexTardeHorarios[1]}\nElis\n{pSexManhaHorarios[1]}\n{pSexTardeHorarios[2]}\n{pSexNoiteHorarios[1]}\nAlice\n{pSexTardeHorarios[0]}\n{pSexNoiteHorarios[0]}")  
+
+agenda=[]
+def agendarHorario():
+    print('''
+|--------QUADRO DE MÉDICOS--------|   
+| (1) MAURO                       |
+| (2) LUCAS                       |
+| (3) ANA                         |
+| (4) RITA                        |
+| (5) CLÁUDIO                     |
+| (6) CARLA                       |
+| (7) ALICE                       |
+| (8) RUAN                        |
+| (9) ANTÔNIO                     |
+| (10) ELIS                       |
+|---------------------------------|
+    ''')
+    
+    x = int(input("Digite o número do médico: "))
+    d = dia()
+    h = input("Digite o horário: ")
+
+    agendamento=[]
+    agendamento.append(x)
+    agendamento.append(d)
+    agendamento.append(h)
+
+    if len(agenda) == 0:
+        agenda.append(agendamento)
+        return "Agendado com sucesso!"
+    else:
+        for i in agenda:
+            if i == agendamento:
+                return "Este horário ocupado!"
+            
+        agenda.append(agendamento)
+        return "Agendado com sucesso!"
+
+while True:
+    a = menu()
+    if a == "A":
+        medicos()
+    elif a == "B":
+        especialidades()
+    elif a == "C":
+        esp = esp()
+        dia = dia()
+        diaEsp(esp, dia)
+    elif a == "D":
+        print(agendarHorario())
+    elif a == "E":
+        False   
+
+
+
+
